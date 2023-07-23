169aa704f51615ea93eaea5df03628e23cffd378
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-07-23 13:47:09 +0700

    change nanoid to shortid

--- src/domain/useCase/controller/UrlController.js
+++ src/domain/useCase/controller/UrlController.js
@@ -1,17 +1,16 @@
-const { nanoid } = require("nanoid");
-
+const shortid = require("shortid");
 
 class UrlController {
     constructor(urlUseCase) {
         this.useCase = urlUseCase;
-    }    
+    }
 
     async createUrl(req, res) {
         const body = req.body;
 
         if(!body.url) return res.status(400).json({error: "Url is required"});
 
-        const shortId = nanoid(8); // generate 8 random string
+        const shortId = shortid(); 
         try {
             const createdUrl = await this.useCase.createUrl(shortId, body.url);
             res.status(200).json({ createdUrl });
