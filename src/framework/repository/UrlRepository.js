53a28011a1a773606cae7e26d4b289222cfc25ef
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-07-26 08:22:56 +0700

    make functional getUrl and getStats

--- src/framework/repository/UrlRepository.js
+++ src/framework/repository/UrlRepository.js
@@ -8,6 +8,34 @@ class UrlRepository {
             console.error(`UrlRepository: ${error}`);
         }
     }
+
+    async getUrl(urlEntity) {
+        try {
+          return await URL.findOneAndUpdate(
+                { shortId: urlEntity.shortId },
+                {
+                    $push: {
+                        visitHistory: {
+                            timestamp: Date.now(),
+                        },
+                    },
+                },
+                { new: true }
+            );
+        } catch (error) {
+            console.error(`UrlRepository: ${error}`);
+        }
+    }
+
+    async getStats(urlEntity) {
+        try {
+            return await URL.findOne({
+                shortId: urlEntity.shortId,
+            });
+        } catch(error) {
+            console.error(`UrlRepository: ${error}`);
+        }
+    }
 }
 
 module.exports = UrlRepository;
