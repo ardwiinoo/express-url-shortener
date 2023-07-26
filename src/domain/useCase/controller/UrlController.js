594360125fbb6cc61b5008af9f1590c552c2c52d
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-07-26 08:21:55 +0700

    make functional getUrl and getStats

--- src/domain/useCase/controller/UrlController.js
+++ src/domain/useCase/controller/UrlController.js
@@ -1,4 +1,5 @@
 const shortid = require("shortid");
+const HttpResponse = require("../../../../src/interface/middleware/HttpResponse");
 
 class UrlController {
     constructor(urlUseCase) {
@@ -13,9 +14,41 @@ class UrlController {
         const shortId = shortid(); 
         try {
             const createdUrl = await this.useCase.createUrl(shortId, body.url);
-            res.status(200).json({ createdUrl });
+            const onSuccess = HttpResponse.onSuccess('Successfully create url', createdUrl);
+            res.status(200).json(onSuccess);
         } catch(error) {
             console.error(`UrlController: ${error}`);
+            const onError = HttpResponse.onError('Failed to create url', 500);
+            res.status(500).json(onError);
+        }
+    }
+
+    async getUrl(req, res) {
+        const shortId = req.params.shortId;
+
+        try {
+            const entryUrl = await this.useCase.getUrl(shortId);
+            res.redirect(entryUrl.redirectUrl);
+        } catch(error) {
+            console.error(`UrlController: ${error}`);
+            const onError = HttpResponse.onError('Failed to get url', 500);
+            res.status(500).json(onError);
+        }
+    }
+
+    async getStats(req, res) {
+        const shortId = req.params.shortId;
+
+        try {
+            const entryStats = await this.useCase.getStats(shortId);
+            res.status(200).json({
+                totalAccess: entryStats.visitHistory.length,
+                stats: entryStats.visitHistory
+            });
+        } catch(error) {
+            console.error(`UrlController: ${error}`);
+            const onError = HttpResponse.onError('Failed to get stats', 500);
+            res.status(500).json(onError);
         }
     }
 }
